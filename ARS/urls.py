235f02908cce9615a580bc44cc00d79db888aad3
asmitherman <asmitherman@mail.csuchico.edu>
2018-02-21 12:31:46 -0800

    renamed project files, set up home view w/basic html elemens

--- ARS/urls.py
+++ ARS/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf.urls import url, include
 from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('', include('Austin.urls')),
+    url('admin/', admin.site.urls),
 ]
